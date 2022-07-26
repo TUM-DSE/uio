bdd734d012ab37ad20da206053e15bfab54935fa
Masanori Misono <m.misono760@gmail.com>
2022-07-26 14:36:24 +0000

    run vm with virtio-serial

---
+++ apps/virtio-serial/run.sh
@@ -0,0 +1,6 @@
+sudo qemu-system-x86_64 \
+    -enable-kvm \
+    -chardev socket,path=/tmp/port0,server=on,wait=off,id=char0 \
+    -device virtio-serial \
+    -device virtconsole,chardev=char0,id=ushell,nr=0 \
+    -kernel build/virtioserial_kvm-x86_64 -nographic
