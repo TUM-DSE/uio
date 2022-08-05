94e181441fe69206026f0d758898d03915c7a952
Masanori Misono <m.misono760@gmail.com>
2022-08-05 15:10:47 +0000

    virtio-console [WIP]

--- apps/virtio-console/main.c
+++ apps/virtio-console/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <uk/console.h>
+
 int main()
 {
+	char ch = uk_console_getc();
+	printf("ch=%c\n", ch);
+	uk_console_putc('a');
+
 	printf("busy loop...\n");
+
 	while (1) {
 		sleep(1);
 	}
