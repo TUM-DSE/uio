6438522cab2d5de0ba31ef26a2c8632fbf9ec492
Masanori Misono <m.misono760@gmail.com>
2022-10-14 14:44:40 +0000

    update

--- apps/count/main.c
+++ apps/count/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int count;
+
+void set_count(int c)
+{
+	count = c;
+}
+
 int main()
 {
-	int i = 0;
+	count = 0;
 	while(1) {
-		printf("%d\n", i);
-		i += 1;
+		printf("%d\n", count);
+		count += 1;
 		sleep(1);
 	}
 	return 0;
