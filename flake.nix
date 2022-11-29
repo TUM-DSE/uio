487c58d83da2729c648a0fbc1022569e730c184d
Pogobanane <aenderboy@gmx.de>
2022-11-29 15:59:12 +0000

    add apps/nginx qemu-nixos-nginx comparison

--- flake.nix
+++ flake.nix
@@ -2,9 +2,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nixos-generators }:
     (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -48,6 +53,32 @@
             pkgs.nginx
 
           ];
+        };
+        packages = {
+          nginx-image = nixos-generators.nixosGenerate {
+            inherit pkgs;
+            modules = [ 
+              ({
+                system.stateVersion = "22.11";
+                networking.firewall.enable = false;
+                users.users.root.password = "password";
+                services.getty.autologinUser = pkgs.lib.mkDefault "root";
+
+                services.nginx = {
+                  enable = true;
+                };
+
+                networking.useDHCP = false;
+                networking.interfaces.ens4.useDHCP = false;
+                networking.interfaces.ens4.ipv4.addresses = [ {
+                  address = "172.44.0.2";
+                  prefixLength = 24;
+                } ];
+                networking.defaultGateway = "172.44.0.1";
+              })
+            ];
+            format = "qcow";
+          };
         };
       }));
 }
