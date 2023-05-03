eee69d86f46f3f517cc097b3639875e42ca55a4b
Masanori Misono <m.misono760@gmail.com>
2023-05-03 15:24:19 +0000

    bpf: update helper functions

--- apps/ubpf_tracer/bpf_prog/bpf_helpers.h
+++ apps/ubpf_tracer/bpf_prog/bpf_helpers.h
@@ -8,11 +8,11 @@
 
 #include <stdint.h>
 
-#define bpf_map_get ((uint64_t(*)(uint64_t key1, uint64_t key2))0)
-#define bpf_map_put ((void (*)(uint64_t key1, uint64_t key2, uint64_t value))1)
-#define bpf_map_del ((void (*)(uint64_t key1, uint64_t key2))2)
+#define bpf_map_get ((uint64_t(*)(uint64_t key1, uint64_t key2))1)
+#define bpf_map_put ((void (*)(uint64_t key1, uint64_t key2, uint64_t value))2)
+#define bpf_map_del ((void (*)(uint64_t key1, uint64_t key2))3)
 
-#define bpf_notify ((void (*)(uint64_t function_address))3)
-#define bpf_get_ret_addr ((uint64_t(*)(const char *function_name))4)
+#define bpf_notify ((void (*)(uint64_t function_address))4)
+#define bpf_get_ret_addr ((uint64_t(*)(const char *function_name))5)
 
 #endif /* BPF_HELPERS_H */
