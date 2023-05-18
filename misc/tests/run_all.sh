5a6c41c64aef90b6ef13848185d4bba86c3b0522
Masanori Misono <m.misono760@gmail.com>
2023-05-18 21:59:48 +0000

    fix

--- misc/tests/run_all.sh
+++ misc/tests/run_all.sh
@@ -6,8 +6,8 @@ sudo python3.9 ./measure_memory.py
 sudo python3.9 ./measure_image.py
 sudo python3.9 ./measure_symsize.py
 
-pytho3.9 ./graphs.py ./measurements/app-latest.tsv
-pytho3.9 ./graphs.py ./measurements/console-latest.tsv
-pytho3.9 ./graphs.py ./measurements/memory-latest.tsv
-pytho3.9 ./graphs.py ./measurements/image-latest.tsv
+python3.9 ./graphs.py ./measurements/app-latest.tsv
+python3.9 ./graphs.py ./measurements/console-latest.tsv
+python3.9 ./graphs.py ./measurements/memory-latest.tsv
+python3.9 ./graphs.py ./measurements/image-latest.tsv
 
