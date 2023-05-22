7568bcfedb390b4920def486d0915e0f112fa423
Masanori Misono <m.misono760@gmail.com>
2023-05-22 11:16:10 +0000

    more stable measurement

--- misc/tests/measure_memory.py
+++ misc/tests/measure_memory.py
@@ -10,6 +10,8 @@ from tqdm import tqdm
 from tempfile import TemporaryDirectory
 from pathlib import Path
 from contextlib import contextmanager
+from collections import defaultdict
+import numpy as np
 
 STATS_PATH = MEASURE_RESULTS.joinpath("memory-stats.json")
 
@@ -24,6 +26,15 @@ def get_memstats_cgroup(cgroup_name: str) -> Tuple[int, int, int]:
     with open(str (path / "pids.current"), "r") as f:
         pids = int(f.read())
     return (current, peak, pids)
+
+SIZE = 10
+WARMUP = 0
+def sample(f: Callable[[], Any], size: int = SIZE, warmup: int = WARMUP) -> List[Any]:
+    ret = []
+    for i in tqdm(range(0, warmup)):
+        f()
+    for i in tqdm(range(0, size)):
+        f()
 
 def measure_memory(helpers: confmeasure.Helpers,
                    stats: Any,
@@ -57,8 +68,6 @@ def measure_memory(helpers: confmeasure.Helpers,
     else:
         raise ValueError(f"unknown app {app}")
 
-    memstats = {}
-
     keys = [
         "last_alloc_size",
         "max_alloc_size",
@@ -72,49 +81,62 @@ def measure_memory(helpers: confmeasure.Helpers,
         "nb_enomem",
     ]
 
-    with TemporaryDirectory() as tempdir_:
-        log = Path(tempdir_) / "qemu.log"
-        with helpers.spawn_qemu(vmspec, log=log, cgroup=True) as vm:
-            time.sleep(2)
+    memstats = defaultdict(list)
 
-            cnt = 0
-            while True:
-                with open(log, "r") as f:
-                    msg = f.read()
-                    if "max_mem_use:" in msg:
-                        break
-                print(".")
-                cnt += 1
-                time.sleep(1)
-                if cnt > 20:
-                    print(msg)
-                    raise ValueError("timeout")
+    def experiment():
+        with TemporaryDirectory() as tempdir_:
+            log = Path(tempdir_) / "qemu.log"
+            with helpers.spawn_qemu(vmspec, log=log, cgroup=True) as vm:
+                time.sleep(2)
 
-            # get the mem stats
-            current, peak, pids = get_memstats_cgroup("ushell")
-            memstats["total_host_mem_current"] = current
-            memstats["total_host_mem_peak"] = peak
+                cnt = 0
+                while True:
+                    with open(log, "r") as f:
+                        msg = f.read()
+                        if "max_mem_use:" in msg:
+                            break
+                    print(".")
+                    cnt += 1
+                    time.sleep(1)
+                    if cnt > 20:
+                        print(msg)
+                        raise ValueError("timeout")
 
-            if shell == "noshell":
-                memstats["total_host_mem_with_shell_current"] = current
-                memstats["total_host_mem_with_shell_peak"] = peak
-            else:
-                # spawn a socat process
-                sh = vm.socat_Popen()
-                time.sleep(1)
                 # get the mem stats
-                current, peak, pids_ = get_memstats_cgroup("ushell")
-                assert pids_ == (pids + 1), f"{pids_} != {pids + 1}"
-                memstats["total_host_mem_with_shell_current"] = current
-                memstats["total_host_mem_with_shell_peak"] = peak
+                current, peak, pids = get_memstats_cgroup("ushell")
+                memstats["total_host_mem_current"] += [current]
+                memstats["total_host_mem_peak"] += [peak]
 
-        with open(log, "r") as f:
-            for l in f.readlines():
-                for k in keys:
-                    if l.startswith(k):
-                        memstats[k] = int(l.split(":")[1])
+                if shell == "noshell":
+                    memstats["total_host_mem_with_shell_current"] += [current]
+                    memstats["total_host_mem_with_shell_peak"] += [peak]
+                else:
+                    # spawn a socat process
+                    sh = vm.socat_Popen()
+                    time.sleep(1)
+                    # get the mem stats
+                    current, peak, pids_ = get_memstats_cgroup("ushell")
+                    assert pids_ == (pids + 1), f"{pids_} != {pids + 1}"
+                    memstats["total_host_mem_with_shell_current"] += [current]
+                    memstats["total_host_mem_with_shell_peak"] += [peak]
 
-    stats[name] = memstats
+            with open(log, "r") as f:
+                for l in f.readlines():
+                    for k in keys:
+                        if l.startswith(k):
+                            memstats[k] += [int(l.split(":")[1])]
+
+            return memstats
+
+    sample(lambda: experiment())
+
+    # choose median
+    memstats_ = {}
+    for k, v in memstats.items():
+        print(f"{k}: {v}, med: {np.median(v)}, mean: {np.mean(v)} std: {np.std(v)}")
+        memstats_[k] = np.median(v)
+
+    stats[name] = memstats_
     util.write_stats(STATS_PATH, stats)
 
 
